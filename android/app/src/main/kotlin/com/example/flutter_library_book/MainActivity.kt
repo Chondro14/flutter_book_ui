61e323767e0df5219aee18f7657369523cafa590
Chondro14 <johnbell903@gmail.com>
2024-06-30 19:49:05 +0700

    Revert "inititialize project flutter book, with home features progress"
    This reverts commit fcb3faa9292e684e4e06a521f8ef223463b0cd52.

--- android/app/src/main/kotlin/com/example/flutter_library_book/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_library_book
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/Module/search/bloc/cubit/search_cubit.dart
+++
@@ -1,4 +0,0 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_library_book/Core/Model/book_model.dart';
-part '../state/search_state.dart';
